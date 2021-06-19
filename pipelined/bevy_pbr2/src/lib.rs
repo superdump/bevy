a7d743f3ecea5aca762ffac741b16cee6c0d7777
Robert Swain <robert.swain@gmail.com>
2021-06-20 00:45:55 +0200

    bevy_pbr2: Ensure RenderCommandQueue is ready for PbrShaders init

--- pipelined/bevy_pbr2/src/lib.rs
+++ pipelined/bevy_pbr2/src/lib.rs
@@ -12,6 +12,7 @@ use bevy_app::prelude::*;
 use bevy_ecs::prelude::*;
 use bevy_render2::{
     core_pipeline,
+    render_command::RenderCommandQueue,
     render_graph::RenderGraph,
     render_phase::{sort_phase_system, DrawFunctions},
     RenderStage,
@@ -47,6 +48,8 @@ impl Plugin for PbrPlugin {
                 sort_phase_system::<ShadowPhase>.system(),
             )
             .add_system_to_stage(RenderStage::Cleanup, render::cleanup_view_lights.system())
+            // FIXME: Hack to ensure RenderCommandQueue is initialized when PbrShaders is being initialized
+            .init_resource::<RenderCommandQueue>()
             .init_resource::<PbrShaders>()
             .init_resource::<ShadowShaders>()
             .init_resource::<MaterialMeta>()
