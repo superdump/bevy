008b494fd48b25ed03c6cc9edca79e1a584ffef7
Robert Swain <robert.swain@gmail.com>
2021-06-23 21:15:29 +0200

    bevy_gltf2: Copy bevy_gltf and get it working with the pipelined-rendering branch

---
+++ examples/3d/load_gltf_pipelined.rs
@@ -0,0 +1,46 @@
+use bevy::{
+    core::Time,
+    ecs::prelude::*,
+    math::{Quat, Vec3},
+    pbr2::{AmbientLight, OmniLightBundle},
+    prelude::{App, AssetServer, SpawnSceneCommands, Transform},
+    render2::{camera::PerspectiveCameraBundle, color::Color},
+    PipelinedDefaultPlugins,
+};
+
+fn main() {
+    App::new()
+        .insert_resource(AmbientLight {
+            color: Color::WHITE,
+            brightness: 1.0 / 5.0f32,
+        })
+        .add_plugins(PipelinedDefaultPlugins)
+        .add_startup_system(setup.system())
+        .add_system(rotator_system.system())
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_scene(asset_server.load("models/FlightHelmet/FlightHelmet.gltf#Scene0"));
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(0.7, 0.7, 1.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
+        ..Default::default()
+    });
+    commands
+        .spawn_bundle(OmniLightBundle {
+            transform: Transform::from_xyz(3.0, 5.0, 3.0),
+            ..Default::default()
+        })
+        .insert(Rotates);
+}
+
+/// this component indicates what entities should rotate
+struct Rotates;
+
+fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotates>>) {
+    for mut transform in query.iter_mut() {
+        *transform = Transform::from_rotation(Quat::from_rotation_y(
+            (4.0 * std::f32::consts::PI / 20.0) * time.delta_seconds(),
+        )) * *transform;
+    }
+}
