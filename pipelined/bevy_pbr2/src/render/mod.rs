348adb9b720087d24a8b5f6a22a95d0cb2431ba3
Robert Swain <robert.swain@gmail.com>
2021-07-07 09:09:23 +0200

    bevy_pbr2: Fix GpuLights min_binding_size

--- pipelined/bevy_pbr2/src/render/mod.rs
+++ pipelined/bevy_pbr2/src/render/mod.rs
@@ -66,9 +66,9 @@ impl FromWorld for PbrShaders {
                     ty: BindingType::Buffer {
                         ty: BufferBindingType::Uniform,
                         has_dynamic_offset: true,
-                        // TODO: change this to ViewUniform::std140_size_static once crevice fixes this!
+                        // TODO: change this to GpuLights::std140_size_static once crevice fixes this!
                         // Context: https://github.com/LPGhatguy/crevice/issues/29
-                        min_binding_size: BufferSize::new(1264),
+                        min_binding_size: BufferSize::new(1024),
                     },
                     count: None,
                 },
