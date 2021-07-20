51d94671663d5829c70be4a079efd3ff00cd9bfc
Robert Swain <robert.swain@gmail.com>
2021-07-20 18:44:38 +0200

    Revert "examples: Factor out flying camera controller"
    This reverts commit 3d7d88d1997f1caccbfbfa8b2ee35790f8a73662.

--- examples/3d/shadow_biases_pipelined.rs
+++ examples/3d/shadow_biases_pipelined.rs
@@ -1,25 +1,34 @@
 use bevy::{
+    core::Time,
     ecs::prelude::*,
-    input::Input,
-    math::{EulerRot, Mat4, Vec3},
+    input::{mouse::MouseMotion, Input},
+    math::{EulerRot, Mat4, Quat, Vec2, Vec3},
     pbr2::{
         DirectionalLight, DirectionalLightBundle, PbrBundle, PointLight, PointLightBundle,
         StandardMaterial,
     },
     prelude::{App, Assets, KeyCode, Transform},
     render2::{
-        camera::{OrthographicProjection, PerspectiveCameraBundle},
+        camera::{Camera, OrthographicProjection, PerspectiveCameraBundle},
         color::Color,
         mesh::{shape, Mesh},
     },
     PipelinedDefaultPlugins,
 };
 
-#[path = "../utils/mod.rs"]
-mod utils;
-use utils::{camera_controller, CameraController};
-
 fn main() {
+    println!(
+        "Controls:
+    WSAD   - forward/back/strafe left/right
+    LShift - 'run'
+    E      - up
+    Q      - down
+    L      - switch between directional and point lights
+    1/2    - decrease/increase point light depth bias
+    3/4    - decrease/increase point light normal bias
+    5/6    - decrease/increase direction light depth bias
+    7/8    - decrease/increase direction light normal bias"
+    );
     App::new()
         .add_plugins(PipelinedDefaultPlugins)
         .add_startup_system(setup.system())
@@ -88,23 +97,13 @@ fn setup(
     });
 
     // camera
-    let controller = CameraController::default();
-    println!(
-        "Controls:
-    L\t- switch between directional and point lights
-    1/2\t- decrease/increase point light depth bias
-    3/4\t- decrease/increase point light normal bias
-    5/6\t- decrease/increase direction light depth bias
-    7/8\t- decrease/increase direction light normal bias"
-    );
-    controller.print_usage();
     commands
         .spawn_bundle(PerspectiveCameraBundle {
             transform: Transform::from_xyz(-1.0, 1.0, 1.0)
                 .looking_at(Vec3::new(-1.0, 1.0, 0.0), Vec3::Y),
             ..Default::default()
         })
-        .insert(controller);
+        .insert(CameraController::default());
 
     for z_i32 in -spawn_plane_depth as i32..=0 {
         commands.spawn_bundle(PbrBundle {
@@ -213,6 +212,123 @@ fn adjust_directional_light_biases(
                 "DirectionalLight shadow_normal_bias: {}",
                 light.shadow_normal_bias
             );
+        }
+    }
+}
+
+struct CameraController {
+    pub enabled: bool,
+    pub sensitivity: f32,
+    pub key_forward: KeyCode,
+    pub key_back: KeyCode,
+    pub key_left: KeyCode,
+    pub key_right: KeyCode,
+    pub key_up: KeyCode,
+    pub key_down: KeyCode,
+    pub key_run: KeyCode,
+    pub walk_speed: f32,
+    pub run_speed: f32,
+    pub friction: f32,
+    pub pitch: f32,
+    pub yaw: f32,
+    pub velocity: Vec3,
+}
+
+impl Default for CameraController {
+    fn default() -> Self {
+        Self {
+            enabled: true,
+            sensitivity: 0.5,
+            key_forward: KeyCode::W,
+            key_back: KeyCode::S,
+            key_left: KeyCode::A,
+            key_right: KeyCode::D,
+            key_up: KeyCode::E,
+            key_down: KeyCode::Q,
+            key_run: KeyCode::LShift,
+            walk_speed: 10.0,
+            run_speed: 30.0,
+            friction: 0.5,
+            pitch: 0.0,
+            yaw: 0.0,
+            velocity: Vec3::ZERO,
+        }
+    }
+}
+
+fn camera_controller(
+    time: Res<Time>,
+    mut mouse_events: EventReader<MouseMotion>,
+    key_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut CameraController), With<Camera>>,
+) {
+    let dt = time.delta_seconds();
+
+    // Handle mouse input
+    let mut mouse_delta = Vec2::ZERO;
+    for mouse_event in mouse_events.iter() {
+        mouse_delta += mouse_event.delta;
+    }
+
+    for (mut transform, mut options) in query.iter_mut() {
+        if !options.enabled {
+            continue;
+        }
+
+        // Handle key input
+        let mut axis_input = Vec3::ZERO;
+        if key_input.pressed(options.key_forward) {
+            axis_input.z += 1.0;
+        }
+        if key_input.pressed(options.key_back) {
+            axis_input.z -= 1.0;
+        }
+        if key_input.pressed(options.key_right) {
+            axis_input.x += 1.0;
+        }
+        if key_input.pressed(options.key_left) {
+            axis_input.x -= 1.0;
+        }
+        if key_input.pressed(options.key_up) {
+            axis_input.y += 1.0;
+        }
+        if key_input.pressed(options.key_down) {
+            axis_input.y -= 1.0;
+        }
+
+        // Apply movement update
+        if axis_input != Vec3::ZERO {
+            let max_speed = if key_input.pressed(options.key_run) {
+                options.run_speed
+            } else {
+                options.walk_speed
+            };
+            options.velocity = axis_input.normalize() * max_speed;
+        } else {
+            let friction = options.friction.clamp(0.0, 1.0);
+            options.velocity *= 1.0 - friction;
+            if options.velocity.length_squared() < 1e-6 {
+                options.velocity = Vec3::ZERO;
+            }
+        }
+        let forward = transform.forward();
+        let right = transform.right();
+        transform.translation += options.velocity.x * dt * right
+            + options.velocity.y * dt * Vec3::Y
+            + options.velocity.z * dt * forward;
+
+        if mouse_delta != Vec2::ZERO {
+            // Apply look update
+            let (pitch, yaw) = (
+                (options.pitch - mouse_delta.y * 0.5 * options.sensitivity * dt).clamp(
+                    -0.99 * std::f32::consts::FRAC_PI_2,
+                    0.99 * std::f32::consts::FRAC_PI_2,
+                ),
+                options.yaw - mouse_delta.x * options.sensitivity * dt,
+            );
+            transform.rotation = Quat::from_euler(EulerRot::ZYX, 0.0, yaw, pitch);
+            options.pitch = pitch;
+            options.yaw = yaw;
         }
     }
 }
