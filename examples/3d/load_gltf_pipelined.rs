aa2ac1107828cfc3b2154be0d9663e4ed6149d17
Robert Swain <robert.swain@gmail.com>
2021-07-20 10:35:50 +0200

    load_gltf_pipelined: Fixes for pipelined-rendering updates

--- examples/3d/load_gltf_pipelined.rs
+++ examples/3d/load_gltf_pipelined.rs
@@ -1,7 +1,7 @@
 use bevy::{
     core::Time,
     ecs::prelude::*,
-    math::Vec3,
+    math::{EulerRot, Quat, Vec3},
     pbr2::{AmbientLight, DirectionalLight, DirectionalLightBundle},
     prelude::{App, AssetServer, SpawnSceneCommands, Transform},
     render2::{
@@ -30,28 +30,33 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         ..Default::default()
     });
     const HALF_SIZE: f32 = 1.0;
-    let mut directional_light = DirectionalLight::default();
-    directional_light.color = Color::WHITE;
-    directional_light.shadow_projection = OrthographicProjection {
-        left: -HALF_SIZE,
-        right: HALF_SIZE,
-        bottom: -HALF_SIZE,
-        top: HALF_SIZE,
-        near: -10.0 * HALF_SIZE,
-        far: 10.0 * HALF_SIZE,
-        ..Default::default()
-    };
-    directional_light.shadow_bias_min = 0.0001;
-    directional_light.shadow_bias_max = 0.001;
     commands.spawn_bundle(DirectionalLightBundle {
-        directional_light,
+        directional_light: DirectionalLight {
+            shadow_projection: OrthographicProjection {
+                left: -HALF_SIZE,
+                right: HALF_SIZE,
+                bottom: -HALF_SIZE,
+                top: HALF_SIZE,
+                near: -10.0 * HALF_SIZE,
+                far: 10.0 * HALF_SIZE,
+                ..Default::default()
+            },
+            ..Default::default()
+        },
         ..Default::default()
     });
 }
 
-fn animate_light_direction(time: Res<Time>, mut query: Query<&mut DirectionalLight>) {
-    for mut light in query.iter_mut() {
-        let (s, c) = (time.seconds_since_startup() as f32 * std::f32::consts::TAU / 10.0).sin_cos();
-        light.set_direction(Vec3::new(2.0 * s, -1.0, c));
+fn animate_light_direction(
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<DirectionalLight>>,
+) {
+    for mut transform in query.iter_mut() {
+        transform.rotation = Quat::from_euler(
+            EulerRot::ZYX,
+            0.0,
+            time.seconds_since_startup() as f32 * std::f32::consts::TAU / 10.0,
+            -std::f32::consts::FRAC_PI_4,
+        );
     }
 }
