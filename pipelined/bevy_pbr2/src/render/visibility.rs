bddb1e74637ca989f76365d55a4ce76f28e09fea
Robert Swain <robert.swain@gmail.com>
2021-09-13 00:05:06 +0200

    Move AABB computation function to Mesh
    And do not apply the GlobalTransform to vertices. This should be handle
    independently and more efficiently.

--- pipelined/bevy_pbr2/src/render/visibility.rs
+++ pipelined/bevy_pbr2/src/render/visibility.rs
@@ -1,60 +1,17 @@
 use bevy_asset::{Assets, Handle};
 use bevy_ecs::prelude::*;
-use bevy_math::{const_vec3, Vec3};
-use bevy_render2::{
-    mesh::{Mesh, VertexAttributeValues},
-    primitives::Aabb,
-};
-use bevy_transform::components::{GlobalTransform, Transform};
+use bevy_render2::{mesh::Mesh, primitives::Aabb};
 
 pub fn calculate_aabbs(
     mut commands: Commands,
     meshes: Res<Assets<Mesh>>,
-    new_meshes: Query<(Entity, &GlobalTransform, &Handle<Mesh>), Without<Aabb>>,
-    moved_meshes: Query<
-        (Entity, &GlobalTransform, &Handle<Mesh>),
-        (Changed<GlobalTransform>, With<Aabb>),
-    >,
+    without_aabb: Query<(Entity, &Handle<Mesh>), Without<Aabb>>,
 ) {
-    for (entity, transform, mesh_handle) in new_meshes.iter().chain(moved_meshes.iter()) {
+    for (entity, mesh_handle) in without_aabb.iter() {
         if let Some(mesh) = meshes.get(mesh_handle) {
-            if let Some(aabb) = mesh_to_aabb(mesh, transform) {
+            if let Some(aabb) = mesh.compute_aabb() {
                 commands.entity(entity).insert(aabb);
             }
         }
     }
-}
-
-const VEC3_MIN: Vec3 = const_vec3!([std::f32::MIN, std::f32::MIN, std::f32::MIN]);
-const VEC3_MAX: Vec3 = const_vec3!([std::f32::MAX, std::f32::MAX, std::f32::MAX]);
-
-pub fn mesh_to_aabb(mesh: &Mesh, transform: &GlobalTransform) -> Option<Aabb> {
-    if let Some(positions) = mesh.attribute(Mesh::ATTRIBUTE_POSITION) {
-        let scale_rotation = Transform {
-            translation: Vec3::ZERO,
-            scale: transform.scale,
-            rotation: transform.rotation,
-        };
-        if let Some(positions) = match positions {
-            VertexAttributeValues::Float32x3(values) => Some(
-                values
-                    .iter()
-                    .map(|coordinates| scale_rotation.mul_vec3(Vec3::from(*coordinates)))
-                    .collect::<Vec<_>>(),
-            ),
-            _ => None,
-        } {
-            let mut minimum = VEC3_MAX;
-            let mut maximum = VEC3_MIN;
-            for p in positions {
-                minimum = minimum.min(p);
-                maximum = maximum.max(p);
-            }
-            if minimum != VEC3_MAX && maximum != VEC3_MIN {
-                return Some(Aabb::from_min_max(minimum, maximum));
-            }
-        }
-    }
-
-    None
 }
