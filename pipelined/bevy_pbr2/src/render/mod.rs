2659dda2b9e220a80bd0d0100d26f2fb6c69555b
Robert Swain <robert.swain@gmail.com>
2021-09-30 20:50:48 +0200

    Various fixes

--- pipelined/bevy_pbr2/src/render/mod.rs
+++ pipelined/bevy_pbr2/src/render/mod.rs
@@ -255,7 +255,7 @@ impl FromWorld for PbrPipeline {
                         has_dynamic_offset: false,
                         // NOTE: Static size for uniform buffers. GpuPointLight has a padded
                         //       size of 128 bytes, so 16384 / 128 = 128 point lights max
-                        min_binding_size: BufferSize::new(0),
+                        min_binding_size: BufferSize::new(16384),
                     },
                     count: None,
                 },
@@ -268,7 +268,7 @@ impl FromWorld for PbrPipeline {
                         has_dynamic_offset: false,
                         // NOTE: With 128 point lights max, indices need 7 bits. Use u8 for
                         //       convenience.
-                        min_binding_size: BufferSize::new(0),
+                        min_binding_size: BufferSize::new(16384),
                     },
                     count: None,
                 },
@@ -283,7 +283,7 @@ impl FromWorld for PbrPipeline {
                         //       The count can be at most all 128 lights so 7 bits.
                         //       Pack the offset into the upper 24 bits and the count into the
                         //       lower 8 bits for convenience.
-                        min_binding_size: BufferSize::new(0),
+                        min_binding_size: BufferSize::new(16384),
                     },
                     count: None,
                 },
